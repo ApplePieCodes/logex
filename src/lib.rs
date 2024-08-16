59c028f2709376b62ed6edfc674b7810f1fd043e
Liam Greenway <ApplePiePc@outlook.com>
2024-08-16 16:11:20 -0400

    Init

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+//! logex: a simple logger for rust apps
+
+use colored::Colorize;
+
+/// Type of log message
+pub enum LogType {
+    Info,
+    Warning,
+    Error,
+    FatalError
+}
+
+/// Prints a log message to the screen in the format [c]: s
+pub fn log(s: &str, c: LogType) {
+    match c {
+        LogType::Info => println!("{}: {}", "[INFO]".bold().bright_white(), s),
+        LogType::Warning => println!("{}: {}", "[WARNING]".bold().yellow(), s),
+        LogType::Error => println!("{}: {}", "[INFO]".bold().red(), s),
+        LogType::FatalError => println!("{}: {}", "[INFO]".bold().truecolor(139, 0, 0), s)
+    }
+}
